fb510feecb51a6aca036e3b71f340fdcdf436618
Boerge Struempfel <bstruempfel@ultratronik.de>
2025-09-15 10:23:10 +0200

    python: Use various flake8 plugins for ruff

--- lua/custom/plugins/lsp/python.lua
+++ lua/custom/plugins/lsp/python.lua
@@ -25,7 +25,36 @@ local function install_pylsp_plugins(pkg)
       'install',
       '-U',
       '--disable-pip-version-check',
+      'eradicate',
+      'flake8-annotations',
+      'flake8-bandit',
+      'flake8-boolean-trap',
+      'flake8-bugbear',
+      'flake8-builtins',
+      'flake8-commas',
+      'flake8-comprehensions',
+      'flake8-datetimez',
+      'flake8-executable',
+      'flake8-fixme',
+      'flake8-future-annotations',
+      'flake8-implicit-str-concat',
+      'flake8-import-conventions',
+      'flake8-logger',
+      'flake8-pie',
+      'flake8-print',
+      'flake8-pyi',
+      'flake8-pytest-style',
+      'flake8-quotes',
+      'flake8-return',
+      'flake8-simplify',
+      'flake8-tidy-imports',
+      'flake8-todos',
+      'flake8-type-checking',
+      'flake8-unused-arguments',
+      'flake8-use-pathlib',
+      'pydoclint',
       'python-lsp-ruff',
+      'pyupgrade',
     },
     cwd = venv,
     env = { VIRTUAL_ENV = venv },
@@ -70,7 +99,51 @@ return {
           pyflakes = { enabled = false },
           pylint = { enabled = false },
           yapf = { enabled = false },
-          ruff = { enabled = true },
+          ruff = {
+            enabled = true,
+            formatEnabled = true,
+            extendSelect = {
+              'A', -- flake8-builtins: Check for python builtins being used as variables or parameters
+              'ANN', -- flake8-annotations: checks for presence of type annotations
+              'ARG', -- flake8-unused-arguments: checks for unused function arguments.
+              'B', -- flake8-bugbear: catches likely bugs and code quality issues
+              'C4', -- flake8-comprehensions: helps you write better list/set/dict comprehensions
+              'COM', -- flake8-commas: checks for trailing commas and proper comma usage
+              'D', -- pydocstyle: check compliance with Python docstring conventions
+              'DOC', -- pydoclint: check if docstrings match the function signature or function implementation.
+              'DTZ', -- flake8-datetimez: Strict require strict timezone manipulation with datetime
+              'E', -- pycodestyle (Error): check code agains code style conventions from pep8
+              'ERA', -- eradicate: detects commented-out or dead code
+              'EXE', -- flake8-executable: Checks permission and shebang related stuff
+              'F', -- pyflakes: detects various errors.
+              'FA', -- flake8-future-annotations: enforces using PEP 563 style future annotations
+              'FBT', -- flake8-boolean-trap: Detect boolean traps.
+              'FIX', -- flake8-fixme: Check for FIXME, TODO and other temporary developer notes.
+              'I', -- Warn on improperly formated Importls
+              'ICN', -- flake8-import-conventions: opinionated plugin how certain packages should be imported or aliased.
+              'ISC', -- flake8-implicit-str-concat: encourage correct string literal concatenation
+              'LOG', -- flake8-logger: checks for issues using the standard library logging module
+              'N', -- pep8-naming: Check against PEP 8 naming conventions
+              'NPY', -- NumPy-specific-rules: some specific numpy rules
+              'PD', -- pandas-vet: opinionated Pandas linting rules
+              'PIE', -- flake8-pie: implements miscellaneous lints for code quality and maintainability
+              'PT', -- flake8-pytest-style: checks common style issues or inconsistencies with pytest-based tests
+              'PTH', -- flake8-use-pathlib: finds use of functions that can be replaced by pathlib module
+              'PYI', -- flake8-pyi: linting for type annotations
+              'Q', -- flake8-quotes: checks quote style
+              'RET', -- flake8-return: checks return values
+              'S', -- flake8-bandit: security issues like unsafe functions or weak crypto
+              'SIM', -- flake8-simplify: helps you simplify your code.
+              'T20', -- flake8-print: print == bad! :)
+              'TC', -- flake8-type-checking: Enforce importing certain types in a TYPE_CHECKING block
+              'TD', -- flake8-todos: check TODOs in the project
+              'TID', -- flake8-tidy-imports: helps you write tidier imports
+              'UP', -- pyupgrade: automatically upgrade syntax for newer versions of the language
+              'W', -- pycodestyle (Warning): check code agains code style conventions from pep8
+            },
+            unsafeFixes = true,
+            preview = false,
+          },
         },
       },
     },
