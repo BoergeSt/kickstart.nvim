1d44b61237a81fb5723a31e3b15d15b27231aed6
Boerge Struempfel <bstruempfel@ultratronik.de>
2025-09-17 11:08:13 +0200

    neo-tree: show all hidden files which are not ignored by git

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -16,6 +16,13 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          '.git',
+        },
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
