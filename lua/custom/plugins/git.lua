86d41973f076031f7e986c29ee081442c8069c57
Boerge Struempfel <bstruempfel@ultratronik.de>
2025-09-22 10:46:18 +0200

    git: Move all git related plugins into one file

---
+++ lua/custom/plugins/git.lua
@@ -0,0 +1,38 @@
+-- All plugins related to git
+return {
+  -- Gitsigns plugin for visualizing the state of the file in git
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      current_line_blame = true,
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  -- Diffview provides an interface for nice diffviews and git history checking
+  {
+    'sindrets/diffview.nvim',
+  },
+  -- Lazygit for quicker git actions
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Open lazy git' },
+    },
+  },
+}
