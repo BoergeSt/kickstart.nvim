6b4ab172039836da11dec85f0c4cfffa02b6c8d1
Boerge Struempfel <boerge.struempfel@gmail.com>
2025-09-30 23:17:27 +0200

    copilot: Switch to copilot.lua for better performance and compatibility

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,15 @@
 return {
-  'github/copilot.vim',
+  -- { 'github/copilot.vim' },
+  { 'zbirenbaum/copilot.lua', dependencies = {
+    'copilotlsp-nvim/copilot-lsp',
+  }, cmd = 'Copilot', event = 'InsertEnter', opts = {} },
+  -- {
+  --   'CopilotC-Nvim/CopilotChat.nvim',
+  --   dependencies = {
+  --     { 'nvim-lua/plenary.nvim', branch = 'master' },
+  --   },
+  --   build = 'make tiktoken',
+  --   opts = { -- See Configuration section for options
+  --   },
+  -- },
 }
