b8d24f809296e9412665a5cee026bec3ff750995
Boerge Struempfel <bstruempfel@ultratronik.de>
2025-09-30 22:18:59 +0200

    dap: Improve readablility of key shortcuts

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -28,54 +28,15 @@ return {
     -- 'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python',
   },
+  -- stylua: ignore
   keys = {
-    {
-      '<F5>',
-      function()
-        require('dap').continue()
-      end,
-      desc = 'Debug: Start/Continue',
-    },
-    {
-      '<F1>',
-      function()
-        require('dap').step_into()
-      end,
-      desc = 'Debug: Step Into',
-    },
-    {
-      '<F2>',
-      function()
-        require('dap').step_over()
-      end,
-      desc = 'Debug: Step Over',
-    },
-    {
-      '<F3>',
-      function()
-        require('dap').step_out()
-      end,
-      desc = 'Debug: Step Out',
-    },
-    {
-      '<leader>b',
-      function()
-        require('dap').toggle_breakpoint()
-      end,
-      desc = 'Debug: Toggle Breakpoint',
-    },
-    {
-      '<leader>B',
-      function()
-        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-      end,
-      desc = 'Debug: Set Breakpoint',
-    },
-    {
-      '<F7>',
-      '<cmd>DapViewToggle!<cr>',
-      desc = 'Debug: See last session result.',
-    },
+    { '<F5>', function() require('dap').continue() end, desc = 'Debug: Start/Continue', },
+    { '<F1>', function() require('dap').step_into() end, desc = 'Debug: Step Into', },
+    { '<F2>', function() require('dap').step_over() end, desc = 'Debug: Step Over', },
+    { '<F3>', function() require('dap').step_out() end, desc = 'Debug: Step Out', },
+    { '<leader>b', function() require('dap').toggle_breakpoint() end, desc = 'Debug: Toggle Breakpoint', },
+    { '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Set Breakpoint', },
+    { '<F7>', '<cmd>DapViewToggle!<cr>', desc = 'Debug: See last session result.', },
   },
   config = function()
     local dap = require 'dap'
