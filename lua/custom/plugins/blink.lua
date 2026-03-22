9333871954cc29eb105d841d245e8282a3cc13bb
Boerge Struempfel <boerge.struempfel@gmail.com>
2026-03-22 21:25:05 +0100

    blink-copilot: switch to fang2hou/blink-copilot implementation

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -42,7 +42,7 @@ return {
       },
       'folke/lazydev.nvim',
       'rcarriga/cmp-dap',
-      'giuxtaposition/blink-cmp-copilot',
+      'fang2hou/blink-copilot',
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -136,18 +136,9 @@ return {
           dap = { name = 'dap', module = 'blink.compat.source' },
           copilot = {
             name = 'copilot',
-            module = 'blink-cmp-copilot',
+            module = 'blink-copilot',
             score_offset = 100,
             async = true,
-            transform_items = function(_, items)
-              local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
-              local kind_idx = #CompletionItemKind + 1
-              CompletionItemKind[kind_idx] = 'Copilot'
-              for _, item in ipairs(items) do
-                item.kind = kind_idx
-              end
-              return items
-            end,
           },
         },
       },
